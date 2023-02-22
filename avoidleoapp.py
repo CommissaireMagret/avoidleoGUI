7680da289c89cac022c6f3538642a1723055f0d5
BuildTools <unconfigured@null.spigotmc.org>
2023-02-22 21:00:16 +0100

    lst_besim_execution_with_hui

--- avoidleoapp.py
+++ avoidleoapp.py
@@ -15,22 +15,45 @@ from PyQt6.QtWidgets import (
     QLineEdit,
     QSpinBox,
 )
-import lstgen
-import besimgen
+import gen_simu_files
 import datetime
 import time
 
 utc_offset = time.localtime().tm_gmtoff
 time_now = time.time()
 time_utc = time_now - utc_offset
+date_time_format = "%d/%m/%Y-%H:%M:%S"
+d_t_format = datetime.datetime.fromtimestamp(time_now + 5 * 60).strftime(date_time_format)
 
 now = QDateTime.currentDateTime()
+
+params = ()
 
 widgets0 = []
 widgets1 = []
 
 
-class Example(QMainWindow):
+def lst():
+    num_seq_value = widgets0[1].text()
+    duration_seq_value = widgets0[3].value()
+    num_slots_value = widgets0[5].value()
+    time_start_value = widgets0[9].dateTime().toSecsSinceEpoch()
+    time_start = datetime.datetime.fromtimestamp(time_start_value).strftime(date_time_format)
+    output_file_value = widgets0[7].text()
+    gen_simu_files.genlst(num_seq_value, duration_seq_value, num_slots_value, time_start, output_file_value)
+
+
+def besim():
+    num_seq_value = widgets0[1].text()
+    duration_seq_value = widgets0[3].value()
+    num_slots_value = widgets0[5].value()
+    time_start_value = widgets0[9].dateTime().toSecsSinceEpoch()
+    time_start = datetime.datetime.fromtimestamp(time_start_value).strftime(date_time_format)
+    output_file_value = widgets0[7].text()
+    gen_simu_files.genjson(num_seq_value, duration_seq_value, num_slots_value, time_start, output_file_value)
+
+
+class AvoidLeoWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -38,13 +61,14 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        global params
         self.setWindowTitle("Avoid LEO SAR satellites")
 
         layout = QVBoxLayout()
         layout0 = QVBoxLayout()
         layout1 = QHBoxLayout()
 
-        self.genparams()
+        params = self.genparams()
         self.buttons()
 
         for w in widgets0:
@@ -85,51 +109,49 @@ class Example(QMainWindow):
     def buttons(self):
         lst_button = QPushButton("Generate .lst for TLS simulator", self)
         lst_button.setCheckable(True)
-        lst_button.clicked.connect(self.lstgen)
         lst_button.resize(lst_button.sizeHint())
         widgets1.append(lst_button)
 
         besim_button = QPushButton("Generate .json for LaReunion besim", self)
         besim_button.setCheckable(True)
-        besim_button.clicked.connect(self.besimgen)
         besim_button.resize(besim_button.sizeHint())
         widgets1.append(besim_button)
 
-    def lstgen(self):
-        # lstgen.genlst(self.num_seq, duration_seq, num_cren, file="default",time_start)
-        pass
-    def besimgen(self):
-        print("besim!")
+        lst_button.clicked.connect(lst)
+        besim_button.clicked.connect(besim)
 
     def genparams(self):
-        num_seq = QLineEdit("6940")
-        seq_label = QLabel("Binary sequence name or number (ex: 6940, etc.):")
+        num_seq = QLineEdit("6940", self)
+        seq_label = QLabel("Binary sequence name or number (ex: 6940, etc.):", self)
         duration_seq = QSpinBox()
-        duration_label = QLabel("Duration of the sequence in minutes:")
+        duration_seq.setValue(10)
+        duration_label = QLabel("Duration of the sequence in minutes:", self)
         num_slots = QSpinBox()
-        slots_label = QLabel("Number of slots/repetition of the sequence:")
-        output_file = QLineEdit("")
-        file_label = QLabel("Name of the output file (without extension):")
+        num_slots.setValue(10)
+        slots_label = QLabel("Number of slots/repetition of the sequence:", self)
+        output_file = QLineEdit("default")
+        file_label = QLabel("Name of the output file (without extension):", self)
         offset = ""
         if utc_offset >= 0:
             offset = "+"
         elif utc_offset < 0:
             offset = "-"
-        offset = offset + str(utc_offset/3600)
-        cal = QDateTimeEdit(QDateTime.fromSecsSinceEpoch(int(time_utc)))
+        offset = offset + str(utc_offset / 3600)
+        cal = QDateTimeEdit(QDateTime.fromSecsSinceEpoch(int(time_utc)), self)
         cal.setCalendarPopup(True)
-        callabel = QLabel("Starting date and UTC time (Your computer is UTC" + offset + "H):")
+        callabel = QLabel("Starting date and UTC time (Your computer is UTC" + offset + "H):", self)
         callabel.setBuddy(cal)
         widgets0.extend([seq_label, num_seq,
                          duration_label, duration_seq,
                          slots_label, num_slots,
                          file_label, output_file,
                          callabel, cal])
+        # return num_seq.text(), duration_seq.value(), num_slots.value, time_start, output_file.text()
 
 
 def main():
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = AvoidLeoWindow()
     sys.exit(app.exec())
 
 
--- gen_simu_files.py
+++ gen_simu_files.py
@@ -8,27 +8,33 @@ time_now = time.time()
 date_time_format = "%d/%m/%Y-%H:%M:%S"
 
 # Coordonées de la base d'émission (lat, long, alt).
-latN = -20.909045
-longE = 55.513512
+
+latN = 43.559281
+longE = 1.487430
 # longW = 360 - longE
-qth = (latN, longE, 100)
-type = "PERSONAL"
+qth = (latN, longE, 146)
 
 # Fichier où sont stockées les séquences binaires du PC de simu pour les émissions.
 file_path = 'C:\Seb\sequences\\'
 
-# Gestion des arguments en ligne de commande.
+type_ = "PERSONAL"
+
+# # Gestion des arguments en ligne de commande.
 # parser = argparse.ArgumentParser()
 # parser.add_argument("num_seq", nargs="?", default="6940", type=str, help='n° séq. bin')
 # parser.add_argument("duration_seq", nargs="?", default="21", type=int, help='durée séq. en min.')
-# parser.add_argument("num_cren", nargs="?", default="20", type=int, help='nombre de créneaux')
+# parser.add_argument("num_cren", nargs="?", default="50", type=int, help='nombre de créneaux')
 # parser.add_argument("file", nargs="?", default="default", type=str, help="nom fichier sortie (sans extension)")
 # parser.add_argument("time_start", nargs="?",
 #                     default=datetime.datetime.fromtimestamp(time_now + 5 * 60).strftime(date_time_format),
 #                     help='date et heure locale de départ (pas UTC) : dd/mm/YYYY hh:mm:ss')
-# parser.add_argument("type_", nargs="?", default="PERSONAL", type=str,
-#                     help='Type : PERSONAL or FACTORY (PERSONAL if nothing specified)')
 # parsed_args = parser.parse_args()
 
 
+def genlst(num_seq, duration_seq, num_cren, time_start, file="default"):
+    # Execution de la fonction pour l'écriture d'un .lst en évitant les LEO.
+    avoidleo.writelst(num_seq, duration_seq, num_cren, file, time_start, file_path, qth)
 
+def genjson(num_seq, duration_seq, num_cren, time_start, file="default"):
+    # Execution de la fonction pour l'écriture d'un .json en évitant les LEO pour le simulateur de La Réunion.
+    avoidleo.writebatch(num_seq, duration_seq, num_cren, file, time_start, type_, qth)
