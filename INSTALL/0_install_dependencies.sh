bac32b562ee0f8e60a4ced504195237887973eb0
CommissaireMagret <83539452+CommissaireMagret@users.noreply.github.com>
2023-02-23 17:49:19 +0100

    Update 0_install_dependencies.sh

--- INSTALL/0_install_dependencies.sh
+++ INSTALL/0_install_dependencies.sh
@@ -6,6 +6,7 @@ apt install python3 -y
 apt install python3-pip -y
 apt install python3-dev -y
 apt install python3-venv -y
+apt install libqt6gui6 -y
 #git clone https://github.com/CommissaireMagret/avoid_leosar.git
 #cd avoid_leosar
 #git fetch
